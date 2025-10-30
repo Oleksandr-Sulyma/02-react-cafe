ce2abf0dec8b10e81e33c33364ca38d80017968d
Oleksandr Sulyma <sulima.ab@gmail.com>
2025-10-30 11:26:33 +0200

    feat: finish React cafe app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,61 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+
+import css from "./App.module.css";
+
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const btnReset = (): boolean => {
+    if (totalVotes > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+  let content;
+  if (totalVotes > 0) {
+    content = (
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
+    );
+  } else {
+    content = <Notification />;
+  }
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={btnReset}
+      />
+      {content}
+    </div>
+  );
+}
